1981bf8bf6929cb479e6760331157a47fbd3bc46
pramestyan <pramesty.anjar@gmail.com>
2018-11-27 11:26:25 +0700

    mvc structure

---
+++ app/src/main/java/com/example/macbookpro/architecturesample/controller/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.macbookpro.architecturesample.controller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.macbookpro.architecturesample.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
