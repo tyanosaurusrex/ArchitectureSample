27a83bb19afd616386aebfb596208e1ba1f8834c
pramestyan <pramesty.anjar@gmail.com>
2018-11-27 10:32:25 +0700

    add

--- app/src/main/java/com/example/macbookpro/architecturesample/view/MainActivity.kt
+++ app/src/main/java/com/example/macbookpro/architecturesample/view/MainActivity.kt
@@ -3,11 +3,13 @@ package com.example.macbookpro.architecturesample.view
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.example.macbookpro.architecturesample.R
+import com.example.macbookpro.architecturesample.model.Board
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val board = Board()
     }
 }
