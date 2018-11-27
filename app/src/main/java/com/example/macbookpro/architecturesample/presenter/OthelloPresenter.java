0ccc1ce9df105adaa28ce08b6569c058283701b7
pramestyan <pramesty.anjar@gmail.com>
2018-11-27 10:33:15 +0700

    add

--- app/src/main/java/com/example/macbookpro/architecturesample/presenter/OthelloPresenter.java
+++ app/src/main/java/com/example/macbookpro/architecturesample/presenter/OthelloPresenter.java
@@ -3,7 +3,6 @@ package com.example.macbookpro.architecturesample.presenter;
 import com.example.macbookpro.architecturesample.model.Board;
 import com.example.macbookpro.architecturesample.model.Cell;
 import com.example.macbookpro.architecturesample.model.Player;
-import com.example.macbookpro.architecturesample.view.MainActivity;
 
 public class OthelloPresenter {
     public MainActivity activity;
