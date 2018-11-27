9cda8eebaf737601b97796668d4e187010f8c10d
pramestyan <pramesty.anjar@gmail.com>
2018-11-27 11:33:10 +0700

    mvvm structure

--- app/src/main/java/com/example/macbookpro/architecturesample/view_model/TicTacToViewModel.java
+++ app/src/main/java/com/example/macbookpro/architecturesample/view_model/TicTacToViewModel.java
@@ -1,11 +1,11 @@
-package com.example.macbookpro.architecturesample.presenter;
+package com.example.macbookpro.architecturesample.view_model;
 
 import com.example.macbookpro.architecturesample.model.Board;
 import com.example.macbookpro.architecturesample.model.Cell;
 import com.example.macbookpro.architecturesample.model.Player;
 import com.example.macbookpro.architecturesample.view.MainActivity;
 
-public class OthelloPresenter {
+public class TicTacToViewModel {
     public MainActivity activity;
 
     public Board board;
